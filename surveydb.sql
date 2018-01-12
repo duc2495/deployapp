90b62dc58f35e5f72319013f288c131e75382f7c
duc2495 <vuongtrungduong1995@gmail.com>
2018-01-12 09:05:23 +0700

    update

--- surveydb.sql
+++ surveydb.sql
@@ -9,9 +9,9 @@ CREATE TABLE customer
   customer_id SERIAL,
   name character varying(100) NOT NULL,
   birthday date NOT NULL,
-  address character varying(100) NOT NULL,
+  address character varying(200) NOT NULL,
   gender character varying(10) NOT NULL,
-  school character varying(100) NOT NULL,
+  school character varying(200) NOT NULL,
   school_year integer NOT NULL,
   CONSTRAINT customer_pkey PRIMARY KEY (customer_id)
 )WITH (
@@ -22,10 +22,11 @@ ALTER TABLE customer
 ;
 
 DROP TABLE IF EXISTS language;
+
 CREATE TABLE language
 (
   language_id SERIAL,
-  language character varying(30) NOT NULL,
+  language character varying(100) NOT NULL,
   CONSTRAINT languages_pkey PRIMARY KEY (language_id)
 )WITH (
   OIDS=FALSE
@@ -34,18 +35,16 @@ ALTER TABLE language
   OWNER TO postgres;
 ;
 
-
 DROP TABLE IF EXISTS customer_language;
+
 CREATE TABLE customer_language
 (
   customer_id integer,
   language_id integer,
   CONSTRAINT customer_language_customer_id_fkey FOREIGN KEY (customer_id)
-      REFERENCES public.customer (customer_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
+      REFERENCES customer (customer_id),
   CONSTRAINT customer_language_language_id_fkey FOREIGN KEY (language_id)
-      REFERENCES public.language (language_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
+      REFERENCES language (language_id)
 )WITH (
   OIDS=FALSE
 );
